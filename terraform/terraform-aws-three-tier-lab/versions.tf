69d547f6d6ae673fc4b2caa7d3fb8db7a658fdff
Ekerette0852 <slylaw4u@gmail.com>
2026-06-30 01:20:01 +0000

    Configure Terraform remote state backend

--- terraform/terraform-aws-three-tier-lab/versions.tf
+++ terraform/terraform-aws-three-tier-lab/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.12.2"
 
+  backend "s3" {
+    bucket         = "ekerette-terraform-state-bucket-136913568034"
+    key            = "terraform/aws-three-tier-lab/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  } 
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
